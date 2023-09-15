70f4683bcabea6ae93651914f30272ad4e1a6655
dudosyka <a.hatson22@gmail.com>
2023-09-16 02:12:41 +0300

    feat(core): kodein implementation
    Kodein for dependency injection

---
+++ src/main/kotlin/com/paprika/utils/kodein/KodeinController.kt
@@ -0,0 +1,23 @@
+package com.paprika.utils.kodein
+
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import org.kodein.di.DIAware
+import org.kodein.di.instance
+
+/**
+ * A [KodeinAware] base class for Controllers handling routes.
+ * It allows to easily get dependencies, and offers some useful extensions.
+ */
+@Suppress("KDocUnresolvedReference")
+abstract class KodeinController : DIAware {
+    /**
+     * Injected dependency with the current [Application].
+     */
+    val application: Application by instance()
+
+    /**
+     * Method that subtypes must override to register the handled [Routing] routes.
+     */
+    abstract fun Routing.registerRoutes()
+}
---
+++ src/main/kotlin/com/paprika/utils/kodein/KodeinService.kt
@@ -0,0 +1,6 @@
+package com.paprika.utils.kodein
+
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+
+open class KodeinService(override val di: DI) : DIAware {}
