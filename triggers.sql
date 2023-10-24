d1e6658f2e79b6c71363a9f9f295afd9331766f2
dudosyka <a.hatson22@gmail.com>
2023-10-24 21:20:47 +0300

    feat(database): triggers update
    iterates over all DishIngredient relations and updates dish cellulose by measureCount from relation

--- triggers.sql
+++ triggers.sql
@@ -1,53 +1,63 @@
 
+create or replace procedure updateDish(dish INT, ingredientId INT, measure INT, sign SMALLINT)
+begin
+    update DishModel set `cellulose` = `cellulose` + sign * (select cellulose from IngredientModel where id = ingredientId) * measure where id = dish;
+end;
+
 create or replace trigger update_dish_ingredient_relation before update on DishIngredientModel for each row begin
-    update DishModel set `cellulose` = `cellulose` - (select cellulose from IngredientModel where id = old.ingredient) * old.measure_count where id = old.dish;
-    update DishModel set `cellulose` = `cellulose` + (select cellulose from IngredientModel where id = new.ingredient) * new.measure_count where id = new.dish;
+    call updateDish(old.dish, old.ingredient, old.measure_count, -1);
+    call updateDish(new.dish, new.ingredient, new.measure_count, 1);
 end;
 
 create or replace trigger remove_dish_ingredient_relation before delete on DishIngredientModel for each row begin
-    update DishModel set `cellulose` = `cellulose` - (select cellulose from IngredientModel where id = old.ingredient) * old.measure_count where id = old.dish;
+    call updateDish(old.dish, old.ingredient, old.measure_count, -1);
 end;
 
 create or replace trigger create_dish_ingredient_relation before insert on DishIngredientModel for each row begin
-    update DishModel set `cellulose` = `cellulose` + (select cellulose from IngredientModel where id = new.ingredient) * new.measure_count where id = new.dish;
+    call updateDish(new.dish, new.ingredient, new.measure_count, 1);
 end;
 
 DELIMITER //
 
-create or replace procedure sub_cellulose_from_all_dishes_with_ingredient(ingredient INT, celluloseCount DOUBLE)
+create or replace procedure sub_cellulose_from_all_dishes_with_ingredient(ingredientId INT, celluloseCount DOUBLE)
 begin
-    DECLARE dish INT;
-    DECLARE measure_count INT;
-    DECLARE cursor_ CURSOR FOR select dish, measure_count from DishIngredientModel where DishIngredientModel.ingredient = ingredient;
-    if (select count(*) from DishIngredientModel where DishIngredientModel.ingredient = ingredient) > 0
-    then
-        fetchDishes: loop fetch cursor_ into dish, measure_count;
-        update DishModel set DishModel.`cellulose` = DishModel.`cellulose` - (celluloseCount * measure_count) where id = dish;
-        end loop;
-    end if;
+    DECLARE done INT DEFAULT 0;
+    DECLARE dishId INT;
+    DECLARE measure DOUBLE;
+    DECLARE cursor_ CURSOR FOR select dish, measure_count from DishIngredientModel where ingredient = ingredientId;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    open cursor_;
+    fetchDishes: loop fetch cursor_ into dishId, measure;
+    IF done = 1 THEN
+        LEAVE fetchDishes;
+    END IF;
+    update DishModel set DishModel.`cellulose` = DishModel.`cellulose` - (celluloseCount * measure) where id = dishId;
+    end loop;
+    close cursor_;
 end;
 
-
-create or replace procedure add_cellulose_to_all_dishes_with_ingredient(ingredient INT, celluloseCount DOUBLE)
+create or replace procedure add_cellulose_to_all_dishes_with_ingredient(ingredientId INT, celluloseCount DOUBLE)
 begin
-    DECLARE dish INT;
-    DECLARE measure_count INT;
-    DECLARE cursor_ CURSOR FOR select dish, measure_count from DishIngredientModel where DishIngredientModel.ingredient = ingredient;
-    if (select count(*) from DishIngredientModel where DishIngredientModel.ingredient = ingredient) > 0
-    then
-        fetchDishes: loop fetch cursor_ into dish, measure_count;
-        update DishModel set DishModel.`cellulose` = DishModel.`cellulose` + (celluloseCount * measure_count) where id = dish;
-        end loop;
-    end if;
+    DECLARE done INT DEFAULT 0;
+    DECLARE dishId INT;
+    DECLARE measure DOUBLE;
+    DECLARE cursor_ CURSOR FOR select dish, measure_count from DishIngredientModel where ingredient = ingredientId;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
+    open cursor_;
+    fetchDishes: loop fetch cursor_ into dishId, measure;
+    IF done = 1 THEN
+        LEAVE fetchDishes;
+    END IF;
+    update DishModel set DishModel.`cellulose` = DishModel.`cellulose` + (celluloseCount * measure) where id = dishId;
+    end loop;
+    close cursor_;
 end;
-
-
 
 DELIMITER ;
 
 create or replace trigger update_ingredient before update on IngredientModel for each row begin
     call sub_cellulose_from_all_dishes_with_ingredient(old.id, old.cellulose);
-    call add_cellulose_to_all_dishes_with_ingredient(old.id, old.cellulose);
+    call add_cellulose_to_all_dishes_with_ingredient(new.id, new.cellulose);
 end;
 
 create or replace trigger remove_ingredient before delete on IngredientModel for each row begin
